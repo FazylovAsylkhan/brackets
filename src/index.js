af19c6edd695aaad3733a203c1a6f923edf58542
Fazylov Asylkhan <fazylov.asylkhan@gmail.com>
2021-02-06 18:48:43 +0500

    feat: Task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    while (str != "") {
+        let flag = false;
+        for (i = 0; i < bracketsConfig.length; i++) {
+            brackets = bracketsConfig[i][0] + bracketsConfig[i][1];
+            if (str.includes(brackets)) {
+                str = str.replace(brackets, "");
+                flag = true;
+            }
+        }
+        if (!flag) break;
+    }
+    if (str === "") return true;
+    else return false;
+};
